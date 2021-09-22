681013a527f7e73b3ab549df3c20b8f2980bb87a
MeinLiX <meinlix@ukr.net>
2021-09-22 17:00:11 +0300

    [Console] add example to interract with db

--- JDBConsole/Program.cs
+++ JDBConsole/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using System.Threading.Tasks;
+using JDBSource;
 
 namespace JDBConsole
 {
     class Program
     {
-        static void Main(string[] args)
+        private const string ConsoleTitle = "JDB | Console DBMS";
+
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Title = ConsoleTitle;
+            Database database = new("super-test");
+            await database.OpenConnection();
+
+            await database.RemoveScheme(database.GetSchemes());
+
+            await database.AddScheme("myDB");
+            await database.AddScheme("myUserDB");
         }
     }
 }
