eb576723ab5b00c60f1a601982be19fd589a1fea
MeinLiX <meinlix@ukr.net>
2021-10-10 23:59:19 +0300

    [WinClient] Add TextRefactor.

---
+++ JDBWinClient/Utils/TextRefactor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace JDBWinClient.Utils
+{
+    internal class TextRefactor
+    {
+        public static bool ValidFiled(string str) => !string.IsNullOrEmpty(str) &&
+                                                     !str.Contains(' ');
+        
+    }
+}
