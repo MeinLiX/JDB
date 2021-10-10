2e87b945238602f30a71cdc08b6baa11dfa62dcf
MeinLiX <meinlix@ukr.net>
2021-10-10 15:23:35 +0300

    [WinClient] Add LambdaCommand

---
+++ JDBWinClient/Utils/Commands/CommandBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace JDBWinClient.Utils.Commands
+{
+    internal abstract class CommandBase : ICommand
+    {
+#pragma warning disable CS8612 // Nullability of reference types in type doesn't match implicitly implemented member.
+        public event EventHandler CanExecuteChanged
+#pragma warning restore CS8612 // Nullability of reference types in type doesn't match implicitly implemented member.
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+#pragma warning disable CS8767 // Nullability of reference types in type of parameter doesn't match implicitly implemented member (possibly because of nullability attributes).
+        public abstract bool CanExecute(object parameter);
+#pragma warning restore CS8767 // Nullability of reference types in type of parameter doesn't match implicitly implemented member (possibly because of nullability attributes).
+
+#pragma warning disable CS8767 // Nullability of reference types in type of parameter doesn't match implicitly implemented member (possibly because of nullability attributes).
+        public abstract void Execute(object parameter);
+#pragma warning restore CS8767 // Nullability of reference types in type of parameter doesn't match implicitly implemented member (possibly because of nullability attributes).
+    }
+}
