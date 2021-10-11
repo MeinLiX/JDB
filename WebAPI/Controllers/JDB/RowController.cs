7e42b0d224abf4726fe9c7beaa2f110d9fbd27da
MeinLiX <meinlix@ukr.net>
2021-10-12 00:31:37 +0300

    [WebAPI] add get request (RowController) list of rows

--- WebAPI/Controllers/JDB/RowController.cs
+++ WebAPI/Controllers/JDB/RowController.cs
@@ -1,4 +1,5 @@
 ﻿using JDBWebAPI.Services;
+using JDBWebAPI.Utils;
 using Microsoft.AspNetCore.Mvc;
 
 namespace JDBWebAPI.Controllers.JDB
@@ -15,5 +16,18 @@ namespace JDBWebAPI.Controllers.JDB
             _logger = logger;
             _dbLogic = dbLogic;
         }
+
+        [HttpGet("database/{database}/scheme/{scheme}/table/{table}/row")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult GetTable(string database, string scheme, string table) => JsonSerialize.ResponseTemplate(
+                () => new JsonResult(
+                    JsonSerialize.Data(new
+                    {
+                        rows = _dbLogic.GetTable(database, scheme, table)?.GetRows()?.Select(row => row.GetAsDictionary()).ToList()
+                    }, $"Execude '{database}'->'{scheme}'->'{table}'."
+                ))
+                {
+                    StatusCode = StatusCodes.Status200OK
+                });
     }
 }
