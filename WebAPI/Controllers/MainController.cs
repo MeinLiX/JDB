5847c6b021d437a9104e933b4dfaec5a27b45fac
MeinLiX <meinlix@ukr.net>
2021-10-11 15:08:45 +0300

    initial WebApi project

---
+++ WebAPI/Controllers/MainController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JDBWebAPI.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class MainController : ControllerBase
+    {
+        private readonly ILogger<MainController> _logger;
+
+        public MainController(ILogger<MainController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
